258df503b4bfccce016e1ec6f666de437a69dd59
KlingerMichSena <mklinger3@misena.edu.co>
2022-12-14 18:09:09 -0500

    models clientes

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Clientes(models.Model):
+    nombre= models.CharField(max_lenght=50, verbose_name="Nombre")
+    apellido= models.CharField(max_lenght=50, verbose_name="Apellido")
+    telefono= models.CharField(max_lenght=50, verbose_name="Teléfono")
