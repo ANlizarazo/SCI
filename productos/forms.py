29ad1982b228449ddc5598567b1c8770108bfa5d
KlingerMichSena <michellklinger@gmail.com>
2022-12-18 23:33:05 -0500

    clase productos

---
+++ productos/forms.py
@@ -0,0 +1,10 @@
+from productos.models import Producto
+
+class ProductoForm(forms.ModelForm):
+    """Form definition for Producto."""
+
+    class Meta:
+        """Meta definition for Productoform."""
+
+        model = Producto
+        fields = '__all__'
--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Productos(models.Model):
+    nombre = models.CharField(max_lenght=60, verbose_name="Nombre")    
+    categorias = models.CharField(max_lenght=50, verbose_name="Categoría")
+    descripcion = models.CharField(max_lenght=100, verbose_name="Descripción")
+    precio = models.CharField(max_lenght=50, verbose_name="Precio")
