65dbd20f0ff1a00e144ada443710ebfea11a8f71
KlingerMichSena <michellklinger@gmail.com>
2023-01-03 00:10:02 -0500

    Update models.py

--- productos/models.py
+++ productos/models.py
@@ -7,3 +7,7 @@ class Productos(models.Model):
     categorias = models.CharField(max_lenght=50, verbose_name="Categoría")
     descripcion = models.CharField(max_lenght=100, verbose_name="Descripción")
     precio = models.CharField(max_lenght=50, verbose_name="Precio")
+    class Estado(models.TextChoices):
+        ACTIVO='1', _('Activo')
+        INACTIVO='0', _('Inactivo')
+    estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
