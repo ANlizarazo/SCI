b441950f632dc31efd381b46e52ef3721141d39d
Alcira <arodriguez7057@misena.edu.co>
2023-01-04 19:41:23 -0500

    .

--- ventas/models.py
+++ ventas/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Ventas(models.Model):
-    id= models.AutoField(primary_key=True)
-    subTotalVenta=models.CharField(max_length=20, verbose_name="subTotalVenta")
-    fecha= models.CharField(max_lenght=50, verbose_name="Fecha")
-    porcentajeIva=models.CharField(max_length=20, verbose_name="porcentajeIva")
-    totalVenta=models.CharField(max_length=20, verbose_name="totalVenta")
-    DetalleVenta=models.CharField(max_length=20, verbose_name="DetalleVenta")
-    Cliente=models.CharField(max_length=20, verbose_name="Cliente")
+    id= models.AutoField(unique= True, primary_key=True)
+    subTotalVenta= models.BigIntegerField(max_length=200, verbose_name="subTotalVenta")
+    fecha= models.DateField(max_lenght=10, verbose_name="Fecha")
+    porcentajeIva= models.bigint(max_length=10, verbose_name="porcentaje Iva")
+    totalVenta= models.BigIntegerField(max_length=200, verbose_name="total Venta")
+    DetalleVenta= models.CharField(max_length=200, verbose_name="Detalle Venta")
+    Cliente= models.CharField(max_length=20, verbose_name="Cliente")
