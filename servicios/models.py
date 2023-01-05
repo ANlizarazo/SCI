88daf67f856a1d693a59da84656accb8a2a737b5
Angela-07 <112147810+Angela-05@users.noreply.github.com>
2023-01-04 21:08:42 -0500

    Update models.py

--- servicios/models.py
+++ servicios/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Servicios(models.Model):
-    tipo= models.CharField(max_lenght=50, verbose_name="Tipo de servicio")
-    responsable= models.CharField(max_lenght=50, verbose_name="Persona responsable")
-    descripcion= models.CharField(max_lenght=120, verbose_name="Descripción del servicio")
-
-
+class Servicio(models.Model):
+    observacion= models.TextField(max_lenght=200, verbose_name="Observación")
+    fechadeInicio= models.DateField(max_lenght=9, verbose_name="Fecha de Inicio")
+    fechadeEntrega= models.DateField(max_lenght=9, verbose_name="Fecha de Entrega")    
+    precio= models.BigIntegerField(max_lenght=100, verbose_name="Precio")
+    observacionFinal= models.TextField(max_lenght=250, verbose_name="Orservación Final")
+    tipodeservicio=models.charfield(max_lengh=50, verbose_name="tipo de servicio")
+    tecnico=models.CharField(max_length=50, verbose_name="Técnico")
