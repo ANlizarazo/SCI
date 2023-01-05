12ad6cc4347869a8cb2a36e91da8f817a21ddf62
Alcira <arodriguez7057@misena.edu.co>
2023-01-04 21:36:02 -0500

    .

--- productos/models.py
+++ productos/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Productos(models.Model):
-    codigo = models . IntegerField ( primary_key = True , unique = True, verbose_name = "Código producto"  )
-    nombre = models.CharField(max_lenght=60, verbose_name="Nombre") 
-    precio = models.CharField(max_lenght=50, verbose_name="Precio")   
-    especificaciones = models.CharField(max_lenght=100, verbose_name="Especificaciones")
+    id = models.Autofield (primary_key = True , unique = True, verbose_name = "ID producto")
+    nombre = models.CharField(max_lenght=50, verbose_name="Nombre") 
+    precio = models.BigIntegerField(validadores = [ MinValueValidator ( 0 )], verbose_name="Precio")   
+    especificaciones = models.CharField(max_lenght=200, verbose_name="Especificaciones")
     class Estado(models.TextChoices):
         ACTIVO='1', _('Activo')
         INACTIVO='0', _('Inactivo')
     estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
-    categorias = models.CharField(max_lenght=50, verbose_name="Categoría")
+    stock = models.BigIntegerField(validadores = [ MinValueValidator ( 0 )], verbose_name="Cantidad") 
+    porcentajeIva= models.BigInt(max_length=10, verbose_name="porcentaje Iva")
+    categoria = models.CharField(max_lenght=50, verbose_name="Categoría")
