1c594d2d409df3de00a2ce3f1fdd8fa6e6203310
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-01-11 18:30:42 -0500

    .

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from usuarios.forms import UsuarioForm
 
 from usuarios.models import Usuario
 
@@ -11,3 +12,20 @@ def usuarios(request):
         "usuarios": usuarios
     }
     return render(request,'usuarios/usuarios.html',context)
+
+def usuarios_crear(request):
+
+    if request.method == "POST":
+        form=UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("El usuario se guardó correctamente")
+            return redirect('usuarios')
+        else:
+            print("El usuario NO se guardó")
+    else:
+        form=UsuarioForm()
+    context={
+            "form":form
+    }
+    return render(request,'usuarios/usuarios-crear.html',context)
--- ventas/views.py
+++ ventas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from ventas.forms import VentaForm
 
 from ventas.models import Venta
 
@@ -11,3 +12,20 @@ def ventas(request):
         "ventas": ventas
     }
     return render(request,'ventas/ventas.html',context)
+
+def ventas_crear(request):
+
+    if request.method == "POST":
+        form=VentaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("La venta se guardó correctamente")
+            return redirect('ventas')
+        else:
+            print("La venta NO se guardó")
+    else:
+        form=VentaForm()
+    context={
+            "form":form
+    }
+    return render(request,'ventas/ventas-crear.html',context)
