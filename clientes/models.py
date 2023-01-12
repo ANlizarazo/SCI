794e024caf35b9af45d4ee2d61a7f094cbb9277c
KlingerMichSena <michellklinger@gmail.com>
2023-01-11 20:39:35 -0500

    correcciones

--- clientes/models.py
+++ clientes/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+from compras.models import Compra
 
 # Create your models here.
 
 
-
+class Ciudad(models.Model):
+    nombre= models.CharField(max_length=60, verbose_name="Ciudad")
 class Cliente(models.Model):
-    nombre= models.CharField(max_length=100, verbose_name="Nombre")
-    apellido= models.CharField(max_length=100, verbose_name="Apellido")
+    nombres= models.CharField(max_length=50, verbose_name="Nombres")
+    apellidos= models.CharField(max_length=50, verbose_name="Apellidos")
     nit= models.CharField(unique=True, max_length=20, verbose_name="NIT")
+    telefono=models.CharField(max_length=20, verbose_name="Teléfono")
+    direccion=models.CharField(max_length=70, verbose_name="Dirección")
+    email=models.CharField(max_length=100, verbose_name="Correo Electrónico")
     class TipoDocumento(models.Model):
         CC='CC', _('Cédula de Ciudadanía')
         CE='CE', _('Cédula de Extranjería')
-        PP='PP', _('Pasaporte')
-        OT= 'Otro', _('Otro Tipo de Documento')
-    tipoDocumento=models.CharField(max_length=4, choices=TipoDocumento.CC, verbose_name="Tipo de Documento")
+        PP='PP', _('Pasaporte')    
+    tipoDocumento=models.CharField(max_length=3, choices=TipoDocumento.CC, verbose_name="Tipo de Documento")
     numDocumento= models.CharField(unique=True, max_length=20, verbose_name="Número de Documento")
-    telefono= models.CharField(max_length=20, verbose_name="Teléfono")
-    direccion=models.CharField(max_length=70, verbose_name="Dirección")
-    email=models.EmailField(max_length=100, verbose_name="Correo Electrónico")
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE, verbose_name="Ciudad")
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, verbose_name="Compra")
     
 
     
--- proveedores/models.py
+++ proveedores/models.py
@@ -13,15 +13,18 @@ class Proveedor(models.Model):
         CE='CE', _('Cédula de Extranjería')
         PP='PP', _('Pasaporte')    
     modoPago=models.CharField(max_length=3, choices=ModoPago.EF, verbose_name="Modo de Pago") 
+    class Rol(models.TextChoices):
+        AD='Admin', _('Administrador')
+        EM='Empl', _('Empleado')
     class Estado(models.TextChoices):
         ACTIVO='1', _('Activo')
-        INACTIVO='Empl', _('Inactivo')
+        INACTIVO='0', _('Inactivo')
     estado=models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
-    rol=models.CharField(max_length=2, choices=Rol.AD, verbose_name="Rol")   
+    rol=models.CharField(max_length=2, choices=Rol.AD, verbose_name="Rol")
     razonsocial= models.CharField(max_length=50, verbose_name="Razón social")
     rut= models.CharField(unique=True, max_length=10, verbose_name="Código RUT")
     ciudad= models.CharField(max_length=50, verbose_name="Ciudad")
     
-    class Devolucion(models.Model):
-    fechaDevolucion=models.DateField(auto_now=True,verbose_name="Fecha Devolución")
-    observaciones=models.TextField(max_length=200, verbose_name="Observaciones")
+    """class Devolucion(models.Model):
+    fechaDevolucion=models.DateField(auto_now=True,verbose_name="Fecha Devolución") 
+    observacion=models.TextField(max_length=200, verbose_name="Observaciones")"""
