191d09b8d4dc73d0ffee6db252571150aba99197
Alcira <arodriguez7057@misena.edu.co>
2023-01-17 21:18:29 -0500

    .

--- productos/urls.py
+++ productos/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from productos.views import productos,
-productos_crear,productos_buscar,productos modificar,productos_listar
-
+from productos.views import productos,productos_crear,productos_buscar,productos_modificar,productos_eliminar
 urlpatterns = [
     path('producto/',productos,name="productos"),
     path('producto-crear/',productos_crear,name="productos-crear"),
     path('producto-buscar/',productos_buscar,name="productos-buscar"),
-    path('producto-modificar/',productos_modificar,name="productos-modificar")
-    path('producto-editar/<int:pk>/',productos_editar,name="productos-editar"),
-    path('producto-eliminar/<int:pk>/',productos_eliminar,name="productos-eliminar"),
-
+    path('producto-modificar/',productos_modificar,name="productos-modificar"),
+    path('producto-editar/<int:pk>/',productos_eliminar,name="productos-eliminar")
+   
 ]
