bc06e6df45ef29499b984d7fd4635d9b52430eff
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-01-21 15:52:00 -0500

    mysql

--- base/settings.py
+++ base/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': 3306,
     }
 }
 
