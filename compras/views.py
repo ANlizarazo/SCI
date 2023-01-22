19114622421816e3ebe0779904e23d4d06db7b24
KlingerMichSena <michellklinger@gmail.com>
2023-01-21 23:41:11 -0500

    actualizaciones

--- compras/views.py
+++ compras/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from compras.forms import CompraForm
+from compras.forms import CompraForm,CompraUpdateForm
 from compras.models import Compra
 
 # Create your views here.
@@ -12,10 +12,25 @@ def compras(request):
     }
     return render(request,'compras/compras.html',context)
 
+def compras_ver(request):
+
+    titulo = "Compras - Ver"
+    if request.method == 'POST':
+        form = CompraForm(request.POST)
+        if form.is_valid():
+            return redirect('compras')
+    else:
+        form = CompraForm()
+    context = {
+        'titulo': titulo,
+        "form": form
+    }
+    return render(request, 'compras/compras-ver.html', context)
+
 def compras_crear(request):
 
     titulo="Compras - Crear"
-    if request.method == 'POST':    
+    if request.method == 'POST'and 'form-crear' in request.POST:    
         form= CompraForm(request.POST)
         if form.is_valid():
             form.save()
@@ -30,3 +45,37 @@ def compras_crear(request):
         "form":form
     }
     return render(request,'compras/compras-crear.html',context)
+
+def compras_modificar(request,pk, *callback_kwargs):
+    titulo = "Compras - Modificar"
+    compra = Compra.objects.get(id=pk)
+    if request.method == "POST" and 'form-modificar' in request.POST:
+        form = CompraForm(request.POST, instance=Compra)
+        modal_status = 'show'
+        pk_compra = request.POST['pk']
+        ## cuerpo del modal ##
+        modal_title = f"Modificar {Compra}"
+        modal_submit = "Modificar"
+        #######################
+        tipo = "modificar"
+        form_update = CompraUpdateForm(instance=Compra)
+        
+        compra = Compra.objects.get(id=pk_compra)
+        if form.is_valid():
+            form.save()
+            return redirect('compras')
+        else:
+            print("Hubo un error al guardar los cambios")
+    else:
+        form = CompraForm(instance=Compra)
+    context = {
+        'titulo': titulo,
+        'form': form,
+        'modal_status':modal_status,
+        'modal_submit': modal_submit,
+        'modal_title': modal_title,
+        'pk': pk_compra,
+        'tipo': tipo,
+        'form_update':form_update
+    }
+    return render(request, 'compras/compras-modificar.html', context)
