65cc9726d04f07236f4b9fe6e48da85182084644
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-01-23 16:10:46 -0500

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -146,9 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mklinger3@misena.edu.co'
-EMAIL_HOST_PASSWORD = 'fbtbdjdjrcppjhzy'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'correoDjango2023@gmail.com'
