ae13facb8863088419019df74a24aa9bc6a624a2
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-01-23 14:12:52 -0500

    .

--- base/settings.py
+++ base/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mklinger3@misena.edu.co'
+EMAIL_HOST_USER = 'correoDjango2023@gmail.com'
 EMAIL_HOST_PASSWORD = 'fbtbdjdjrcppjhzy'
 EMAIL_USE_TLS = True
 
