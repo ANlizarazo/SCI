3c748fee19eb330f52c77a6ebc5bc12a499fe1bb
KlingerMichSena <michellklinger@gmail.com>
2023-01-23 13:13:59 -0500

    correcciones

--- base/settings.py
+++ base/settings.py
@@ -147,8 +147,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mklinger3@misena.edu.co'
-EMAIL_HOST_PASSWORD = 'fbtbdjdjrcppjhzy'
+EMAIL_HOST_USER = 'correoDjango2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'zhceexpqwrnnhwdl'
 EMAIL_USE_TLS = True
 
 LOGIN_URL= 'inicio'
