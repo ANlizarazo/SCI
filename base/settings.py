473c9b291928a55e660e04a158874dca54800e24
Alcira <arodriguez7057@misena.edu.co>
2023-01-23 21:14:41 -0500

    .

--- base/settings.py
+++ base/settings.py
@@ -91,10 +91,9 @@ WSGI_APPLICATION = 'base.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
