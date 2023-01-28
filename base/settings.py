ca8e55910b842b711a8123c6616a5929dbd30db9
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-01-27 23:10:34 -0500

    formulario de crear usuario y venta

--- base/settings.py
+++ base/settings.py
@@ -149,14 +149,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'correoDjango2023@gmail.com'
-EMAIL_HOST_PASSWORD = 'zhceexpqwrnnhwdl'
+EMAIL_HOST_USER = 'correodjango2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'iacsgerxgjjidbcu'
+EMAIL_USE_TLS = True
 
 
 LOGIN_REDIRECT_URL = reverse_lazy('inicio')
