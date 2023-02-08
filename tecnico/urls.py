5710be77e5fa11fe115f70a4828aec946ac5a579
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-02-07 21:51:18 -0500

    boton eliminar y views de tecnico

--- tecnico/urls.py
+++ tecnico/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from tecnico.views import tecnicos,tecnicos_crear, tecnicos_modificar, tecnicos_ver
+from . import views
+
 urlpatterns = [
-    path('',tecnicos,name="tecnicos"),
-    path('tecnico-crear/',tecnicos_crear,name="tecnico-crear"),
-    path('tecnico-ver/',tecnicos_ver,name="tecnico-ver"),
-    path('tecnico-modificar/',tecnicos_modificar,name="tecnico-modificar"),
+    path('',views.tecnicos,name="tecnico"),
+    #Path to ADD tecnico
+    path('tecnico_crear',views.tecnico_crear,name="tecnico_crear"),
+    #Path to EDIT tecnico
+    path('tecnico_modificar',views.tecnico_modificar,name="tecnico_modificar"),
+    #Path to View tecnico data individually
+    path('tecnico/tecnico_ver/<str:tecnico_id>',views.tecnico_ver,name="tecnico_ver"),
+    #Path to DELETE tecnico
+    path('tecnico/tecnico/delete_tecnico/<int:tecnico_id>', views.delete_tecnico, name='delete_tecnico')
 ]
--- tecnico/views.py
+++ tecnico/views.py
@@ -85,3 +85,79 @@ def tecnicos_modificar(request,pk, *callback_kwargs):
         'form_update':form_update
     }
     return render(request, 'tecnico/tecnico-modificar.html', context)
+
+    from django.shortcuts import redirect, render
+from tecnico.models import Tecnico
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+
+# Create your views here.
+
+def tecnicos (request):
+    
+    #importar los tecnicos desde el modulo admin
+    tecnicos_list= Tecnico.objects.all()
+
+    return render(request,'tecnico/tecnico.html',  {"tecnico": tecnicos_list})
+
+#Function to ADD tecnico
+def tecnico_crear(request):
+    if request.method=="POST":
+        if request.POST.get('nombres') \
+            and request.POST.get('apellidos') \
+            and request.POST.get('telefono') \
+            and request.POST.get('genero') \
+            and request.POST.get('tipoDocumento') \
+            and request.POST.get('numDocumento') \
+            and request.POST.get('estado') \
+            and request.POST.get('ciudad'):
+            tecnico= Tecnico()  
+            tecnico.nombres= request.POST.get('nombres')
+            tecnico.apellidos= request.POST.get('apellidos')
+            tecnico.telefono= request.POST.get('telefono')
+            tecnico.genero= request.POST.get('genero')
+            tecnico.tipoDocumento= request.POST.get('tipoDocumento')
+            tecnico.numDocumento= request.POST.get('numDocumento')
+            tecnico.estado= request.POST.get('estado')
+            tecnico.estado= request.POST.get('ciudad')
+            tecnico.save()
+            messages.success(request, "Tecnico añadido con éxito!")
+            return redirect('tecnico')
+        else:
+            messages.error(request, "La creación del tecnico ha fallido!")
+            return redirect('tecnico')
+
+#Function to View tecnico data individually
+def tecnico_ver(request, tecnico_id):
+    tecnico = Tecnico.objects.get( id = tecnico_id) 
+    if tecnico != None:
+        return render(request, "tecnico/tecnico-modificar.html", {'tecnico':tecnico} )
+    else:
+        return redirect('tecnico/tecnico-ver.html')
+
+#Function to EDIT tecnico
+def tecnico_modificar(request):
+    if request.method == "POST":
+        tecnico = Tecnico.objects.get(id = request.POST.get('id'))
+        if tecnico != None: 
+            tecnico.nombres= request.POST.get('nombres')
+            tecnico.apellidos= request.POST.get('apellidos')
+            tecnico.telefono= request.POST.get('telefono')
+            tecnico.genero= request.POST.get('genero')
+            tecnico.tipoDocumento= request.POST.get('tipoDocumento')
+            tecnico.numDocumento= request.POST.get('numDocumento')
+            tecnico.estado= request.POST.get('estado')
+            tecnico.estado= request.POST.get('ciudad')
+            tecnico.save()
+            messages.success(request, "Técnico Actualizado con éxito!")
+            return HttpResponseRedirect("tecnico/")
+
+#Function to DELETE tecnico
+def delete_tecnico(request, tecnico_id):
+    if request.method == "POST":
+        tecnico = Tecnico.objects.get(id= tecnico_id)
+        tecnico.delete()
+        messages.success(request, "Técnico eliminado satisfactoriamente!")
+        return redirect("tecnico")
+
+
