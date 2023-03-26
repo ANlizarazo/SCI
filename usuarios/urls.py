b30b04469295d54e007515b2037a6a74bf9a5536
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-03-26 14:43:46 -0500

    contraseña y tablas
    se realiza correccion del modulo de recuperar contraseña y se hace funcional todos los botones de la datatable excepto modificar que falta hacer unos ajustes

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-from usuarios.views import recuperar, recuperar_usuarios, usuario_crear, usuario_editar, usuario_modificar, usuario_ver, usuarios, usuarios_eliminar
+from usuarios.views import recuperar, recuperar_usuarios, usuario_crear, usuario_modificar, usuario_ver, usuarios, usuarios_eliminar
 
 urlpatterns = [
     path('',usuarios,name="usuarios"),
     #Path to ADD usuario
     path('usuario_crear',usuario_crear,name="usuario_crear"),
     #Path to EDIT usuario
-    path('usuario_modificar',usuario_modificar,name="usuario_modificar"),
+    path('modificar/<int:pk>',usuario_modificar,name="usuario_modificar"),
     #Path to View usuario data individually
-    path('usuarios/usuario_ver/<str:usuario_id>',usuario_ver,name="usuario_ver"),
+    path('ver/<int:pk>>',usuario_ver,name="usuario_ver"),
     #Path to DELETE usuario
     path('usuarios_eliminar/<int:pk>/', usuarios_eliminar, name="usuarios_eliminar"),
     #Path to Recuperar usuarios
     path('usuarios_recuperar/', recuperar_usuarios, name="recuperar_usuarios"),
-    path('recuperar-usuario/<int:pk>/', recuperar, name='recuperar-usuario'),
-    path('editar/<int:pk>', usuario_editar, name = 'usuario-editar'),
+    path('recuperar/<int:pk>/', recuperar, name='recuperar_usuario'),
 ]
