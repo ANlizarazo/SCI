138a7e7fd2f19c678f389fabfddfbbc2a38cf968
Alcira <arodriguez7057@misena.edu.co>
2023-04-21 10:56:08 -0500

    .
    .

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from clientes.views import clientes,clientes_crear,clientes_modificar,clientes_ver
+from clientes.views import clientes,clientes_crear,clientes_modificar,clientes_ver, ciudad_crear
 
 
 urlpatterns = [
     path('',clientes,name="clientes"),
     path('clientes-crear/',clientes_crear,name="clientes-crear"),
     path('clientes-modificar/',clientes_modificar,name="clientes-modificar"),
-    path('clientes-ver/',clientes_ver,name="clientes-ver")
+    path('clientes-ver/',clientes_ver,name="clientes-ver"),
+    path('clientes-ciudad', ciudad_crear, name='ciudad-crear')
 ]
