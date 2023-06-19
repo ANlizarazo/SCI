da0755156d3cb6ca3db82910085fdcb57b33b31f
KlingerMichSena <michellklinger@gmail.com>
2023-06-19 17:33:54 -0500

    Correcciones clientes
    Se le agregó el title a gran parte de los botones y se arreglaron errores en el html de clientes

--- compras/models.py
+++ compras/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.utils.formats import get_format
-from proveedores.models import Proveedor
 from usuarios.models import Usuario
-# Create your models here.
+from proveedores.models import Material, Proveedor
+
+
+
 
 class DetalleCompra(models.Model):
     cantidadMaterial = models.BigIntegerField(validators = [ MinValueValidator ( 0 )], verbose_name="Cantidad Material") 
     valorTotalMaterial = models.BigIntegerField(validators = [ MinValueValidator ( 0 )], verbose_name="Valor Total Material")
-    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, verbose_name="Proveedor",null=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name="Usuario",null=True)
+    material= models.ForeignKey(Material, on_delete=models.CASCADE, verbose_name="Material",null=True)
     
     def _str_(self)->str:
-        return "%s %s %s %s" %(self.id,self.proveedor,self.cantidadMaterial,self.valorTotalMaterial)  
+        return "%s %s %s %s" %(self.id,self.usuario,self.cantidadMaterial,self.valorTotalMaterial,self.material)  
 
     class Meta:
         verbose_name = 'Detalle Compra'
@@ -19,14 +22,21 @@ class DetalleCompra(models.Model):
 
 
 class Compra(models.Model):
-    fecha= models.DateTimeField(verbose_name="Fecha Compra")
+    fecha= models.DateField(verbose_name="Fecha",help_text= "DD/MM/AAA")
     detalleCompra = models.ForeignKey(DetalleCompra, on_delete=models.CASCADE, verbose_name="Detalle Compra",null=True)
-    usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name="Usuario",null=True)
+    proveedor=models.ForeignKey(Proveedor, on_delete=models.CASCADE, verbose_name="Proveedor",null=True)
+    class Estado(models.TextChoices):
+        ACTIVO='Activo', ('Activo')
+        INACTIVO='Inactivo', ('Inactivo')
+    estado=models.CharField(max_length=10, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado", blank=True, null=True)
+    
 
     def _str_(self)->str:
-        return "%s %s" %(self.id,self.fecha)  
+        return "%s %s" %(self.id,self.fecha,self.detalleCompra,self.proveedor,self.estado)  
 
     class Meta:
         ordering = ['fecha']
         verbose_name = 'Compra'
         verbose_name_plural = 'Compras'
+
+
--- compras/urls.py
+++ compras/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from compras.views import compras, compras_crear,compras_modificar,compras_ver
+from compras.views import recuperar, recuperar_compras, compras_crear, compras_modificar, \
+    compras_ver, compras, compras_eliminar
 
 urlpatterns = [
     path('',compras,name="compras"),
     path('compras-crear/',compras_crear,name="compras-crear"),
+    path('compras-modificar/<int:pk>',compras_modificar,name="compras-modificar"),
     path('compras-ver/',compras_ver,name="compras-ver"),
-    path('compras-modificar/',compras_modificar,name="compras-modificar"),
+    path('compras_eliminar/<int:pk>/', compras_eliminar, name="compras_eliminar"),
+    path('compras_recuperar/', recuperar_compras, name="recuperar_compras"),
+    path('recuperar/<int:pk>/', recuperar, name='recuperar_compra'),
 ]
