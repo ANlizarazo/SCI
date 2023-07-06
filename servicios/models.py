e5e58a85667822ca1a1fc6f792cc12f8b62b0f31
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2023-07-05 22:36:31 -0500

    updates

--- servicios/models.py
+++ servicios/models.py
@@ -9,21 +9,20 @@ from tipodeservicio.models import TipoServicio
 
 
 class Servicio(models.Model):
-    fecha= models.DateTimeField(verbose_name="Fecha", auto_now_add=True, editable=False, )
+    fecha= models.DateTimeField(verbose_name="Fecha", auto_now_add=True, editable=False,null=True)
     observacion= models.TextField(max_length=300,blank= True,null= True, verbose_name="Observación")
     observacionFinal= models.TextField(max_length=300,blank= True,null= True, verbose_name="Observación Final")
     class Iva(models.TextChoices):
-        BYS='5', _('Bienes y Servicios')
-        GR='19', _('General')    
+        BYS='5', _('Bienes y Servicios')   
         EX='0', _('Exento')
-    porcentajeIva=models.DecimalField(validators=[MinValueValidator(0.0)], decimal_places=1, max_digits=20,max_length=3, choices=Iva.choices, default=Iva.GR, verbose_name="IVA")
+    porcentajeIva=models.DecimalField(validators=[MinValueValidator(0.0)], decimal_places=1, max_digits=20, max_length=2, choices=Iva.choices, default=Iva.BYS, verbose_name="IVA")
     valorTotal= models.PositiveBigIntegerField(validators = [ MinValueValidator ( 0 )], verbose_name="Valor Total")
     class Estado(models.TextChoices):
         ACTIVO='1', _('Activo')
         INACTIVO='0', _('Inactivo')   
-    estado= models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    estado= models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado",null=True)
     tipoServicio= models.ForeignKey(TipoServicio, on_delete=models.CASCADE, verbose_name="Tipo de Servicio",null=True)
-    ciudad= models.ForeignKey(Ciudad, on_delete=models.CASCADE, verbose_name="Ciudad")
+    ciudad= models.ForeignKey(Ciudad, on_delete=models.CASCADE, verbose_name="Ciudad",null=True)
     tecnico= models.ForeignKey(Tecnico, on_delete=models.CASCADE, verbose_name="Técnico", blank=True, null=True)
     
     def __str__(self)->str:
